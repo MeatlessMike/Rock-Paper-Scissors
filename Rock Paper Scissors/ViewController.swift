a4df0ca3b3eb81d2ee565818c359bbfe95811fe2
Akivah <akivahlaviolette@gmail.com>
2022-11-18 10:36:51 -0700

    got background code on

--- Rock Paper Scissors/ViewController.swift
+++ Rock Paper Scissors/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        assignbackground()
     }
-
-
+    
+   
+    
+    
+    func assignbackground()
+    {
+        let background = UIImage(named: "")
+        
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+    }
+    
+    
 }
+
 
